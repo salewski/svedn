393e6c99b9951db15a3c4ed6c0d687254290ea6a
fogus <mefogus@gmail.com>
2016-10-31 16:19:01 -0400

    first blush of entityify

--- src/fogus/sveden/core.clj
+++ src/fogus/sveden/core.clj
@@ -1,13 +1,21 @@
 (ns fogus.sveden.core
   (:require [clojure.data.csv :as csv]
-            [clojure.java.io  :as io]))
+            [clojure.java.io  :as io]
+            [clojure.edn      :as edn]))
 
+(defn ^:private entityify [[head & data]]
+  (let [headers (map edn/read-string head)]
+    (map #(apply hash-map (interleave headers %)) data)))
 
 (comment
 
-  (with-open [in-file (io/reader "./samples/books.csv")]
-    (doall
-     (csv/read-csv in-file)))
+  (def d
+    (with-open [in-file (io/reader "./samples/books.csv")]
+      (doall
+       (csv/read-csv in-file))))
+
+(entityify d)
+
 
 
 )
