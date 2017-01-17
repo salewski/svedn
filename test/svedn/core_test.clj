ef0f95bc9409381c290d4941cea40e55a84ca86b
fogus <mefogus@gmail.com>
2017-01-17 14:17:20 -0500

    Added tests for :whitelist

--- test/svedn/core_test.clj
+++ test/svedn/core_test.clj
@@ -17,8 +17,19 @@
            :book/author     (c/set-of string?)})
 
 (deftest test-whitelist
-  (let [data (svedn/read "./samples/books.csv")]
-    (is (= #{} data))))
+  (let [data0 (svedn/read "./samples/books.csv")
+        data1 (svedn/read "./samples/books.csv"
+                          :conformers CONF
+                          :whitelist  #{:book/author})
+        data (svedn/read "./samples/books.csv"
+                         :conformers CONF
+                         :whitelist  (set (keys CONF)))]
+    (is (empty? data0))
+    (is (not (empty? data)))
+    (is (= #{:book/author}
+           (->> data1 (mapcat keys) set)))
+    (is (= #{:personal/genre :book/author :book/genre :personal/rating}
+           (->> data (mapcat keys) set)))))
 
 (deftest test-conformers
   (let [data (svedn/read "./samples/books.csv"
