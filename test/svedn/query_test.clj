d5cbefd8589bc9f22c4d7d70abecb62fed0b0dc7
fogus <mefogus@gmail.com>
2017-01-18 10:15:24 -0500

    Adding query tests

--- test/svedn/query_test.clj
+++ test/svedn/query_test.clj
@@ -1,4 +1,4 @@
-(ns svedn.core-test
+(ns svedn.query-test
   (:require [clojure.test :refer :all]
             [fogus.svedn :as svedn]
             [fogus.svedn.conformers :as c]
@@ -23,5 +23,15 @@
                          :metadata   :book/meta
                          :amendments :book/amendments)]
     (is (not= #{} (query/on-value #(= % "Magister Ludi") data)))
-    (is (not= #{} (query/on-value :book/title #(= % "House of Leaves") data)))))
+    (is (not= #{} (query/on-value :book/title #(= % "House of Leaves") data)))
+    (is (not= #{} (query/on-value (query/partial-enum :fiction.philosophy) data)))
+    (is (= #{} (query/on-value #(= % :clojure.spec/invalid) data)))))
 
+(deftest test-partial-enum
+  (let [philos? (query/partial-enum :philosophy)
+        f-philos? (query/partial-enum :fiction.philosophy)]
+    (is (philos? :fiction.philosophy))
+    (is (philos? :philosophy/existentialism))
+    (is (f-philos? :fiction.philosophy/slice-of-life))
+    (is (f-philos? :fiction.philosophy))
+    (is (not (philos? :fiction.found-drama)))))
