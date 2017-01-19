83875a1fc1d21d30620de102abbf6c09df980129
fogus <mefogus@gmail.com>
2017-01-19 10:04:33 -0500

    Spec for opts

---
+++ src/fogus/svedn/specs.clj
@@ -0,0 +1,12 @@
+(ns fogus.svedn.specs
+  (:require [clojure.spec :as s]))
+
+(s/def ::conformers-map map?)
+(s/def ::conformers ::conformers-map)
+(s/def ::whitelist  (s/coll-of keyword? :distinct true :into #{}))
+(s/def ::metadata keyword?)
+(s/def ::amendments keyword?)
+
+(s/def ::svedn-opts (s/keys :req-un [::conformers ::whitelist]
+                            :opt-un [::metadata ::amendments]))
+
