d9057a70da05aa70549291640514634d4fc9e470
fogus <mefogus@gmail.com>
2017-09-20 14:15:46 -0400

    Move up to version 0.1.0 of tafl

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject fogus/svedn "0.1.0-SNAPSHOT"
+(defproject fogus/svedn "0.1.0"
   :description "Tools for working with CSV/EDN hybrid data."
   :url "http://www.github.com/fogus/svedn"
   :license {:name "Eclipse Public License"
@@ -6,10 +6,12 @@
 
   :dependencies [[org.clojure/clojure "1.9.0-alpha14"]
                  [org.clojure/data.csv "0.1.3"]
-                 [fogus/tafl "0.1.0-SNAPSHOT"]]
+                 [fogus/tafl "0.1.0"]]
 
   :profiles {:debug {}
              :dev {:resource-paths ["samples"]
                    :dependencies [[com.datomic/datomic-free "0.9.5404"]]}
              :uberjar {:aot :all}
-             :repl {}})
+             :repl {}}
+  :repositories [["snapshots" "https://oss.sonatype.org/content/repositories/snapshots/"]]
+  :deploy-repositories [["clojars" {:sign-releases false}]])
